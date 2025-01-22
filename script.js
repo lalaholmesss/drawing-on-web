2aafb5c17f646a357fac7710de41ec6c1e99a428
Qiyas <107604130+qiyascc@users.noreply.github.com>
2025-01-22 19:49:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,92 @@
-const canvas = document.querySelector("canvas");
-const clear = document.getElementById("clear");
-const slider = document.getElementById("size-slider");
-const colors = document.querySelectorAll("colors");
+class DrawingApp {
+    constructor(canvasSelector, controls) {
+        this.canvas = document.querySelector(canvasSelector);
+        this.ctx = this.canvas.getContext("2d");
+        
+        this.controls = {
+            clearButton: document.getElementById(controls.clearButton),
+            sizeSlider: document.getElementById(controls.sizeSlider),
+            colorInputs: document.querySelectorAll(controls.colorInputs),
+            eraserButton: document.querySelector(controls.eraserButton),
+        };
 
-ctx = canvas.getContext("2d");
+        this.isDrawing = false;
+        this.brushWidth = 5;
+        this.currentColor = "#000000";
+        this.isEraser = false;
+        this.backgroundColor = "#FFFFFF";
 
-let isDrawing = false;
-let brushWidth = 5;
-window.addEventListener("load", () => {
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-})
+        this.initCanvas();
+        this.addEventListeners();
+    }
 
-const startDrawing = () => {
-    isDrawing = true;
-    ctx.beginPath(); // creating a new path to draw so it doesn't start drawing from the 
-                     // last end point each time
-    ctx.lineWidth = brushWidth;
+    initCanvas() {
+        this.canvas.width = this.canvas.offsetWidth;
+        this.canvas.height = this.canvas.offsetHeight;
+        this.ctx.fillStyle = this.backgroundColor;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    addEventListeners() {
+        this.canvas.addEventListener("mousedown", (e) => this.startDrawing(e));
+        this.canvas.addEventListener("mouseup", () => this.stopDrawing());
+        this.canvas.addEventListener("mousemove", (e) => this.draw(e));
+
+        this.controls.colorInputs.forEach((colorInput) => {
+            colorInput.addEventListener("click", () => this.changeColor(colorInput));
+        });
+        this.controls.eraserButton.addEventListener("click", () => this.toggleEraser());
+        this.controls.clearButton.addEventListener("click", () => this.clearCanvas());
+        this.controls.sizeSlider.addEventListener("change", (e) => this.updateBrushSize(e));
+    }
+
+    startDrawing(e) {
+        this.isDrawing = true;
+        this.ctx.beginPath();
+        this.ctx.lineWidth = this.brushWidth;
+        this.ctx.lineCap = "round";
+        this.ctx.strokeStyle = this.isEraser ? this.backgroundColor : this.currentColor;
+        this.ctx.moveTo(e.offsetX, e.offsetY);
+    }
+
+    stopDrawing() {
+        this.isDrawing = false;
+        this.ctx.closePath();
+    }
+
+    draw(e) {
+        if (!this.isDrawing) return;
+        this.ctx.lineTo(e.offsetX, e.offsetY);
+        this.ctx.stroke();
+    }
+
+    changeColor(colorInput) {
+        this.isEraser = false;
+        if (colorInput.tagName === "DIV") {
+            this.currentColor = getComputedStyle(colorInput).backgroundColor;
+        } else if (colorInput.tagName === "INPUT") {
+            this.currentColor = colorInput.value;
+        }
+    }
+
+    toggleEraser() {
+        this.isEraser = true;
+    }
+
+    clearCanvas() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillStyle = this.backgroundColor;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    updateBrushSize(e) {
+        this.brushWidth = e.target.value;
+    }
 }
 
-const stopDrawing = () => {
-    isDrawing = false;
-}
-const drawing = (e) => {
-    if(!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY); // create the line
-    console.log(e.offsetX, e.offsetY); // draw
-    ctx.stroke()
-}
-
-canvas.addEventListener("mousedown", startDrawing);
-canvas.addEventListener("mouseup", stopDrawing);
-canvas.addEventListener("mousemove", drawing);
-slider.addEventListener("change", () => brushWidth = slider.value);
-
+const app = new DrawingApp("canvas", {
+    clearButton: "clear",
+    sizeSlider: "size-slider",
+    colorInputs: ".colors div, .colors input[type='color']",
+    eraserButton: ".fa-eraser",
+});
